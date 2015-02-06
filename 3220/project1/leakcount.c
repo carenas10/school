97500bdac0bbd093011001a47e05b2f7adbeadd5
Jake Dawkins <dawkinsjh@gmail.com>
2015-02-06 15:49:42 -0500

    fixed but on cpsc machines

---
+++ 3220/project1/leakcount.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+    //check cmd line args
+    if(argc != 2){
+        printf("Usage: %s <command>",argv[0]);
+        exit(1);
+    }
+
+
+
+
+
+
+
+return 0;
+}
